24b3c4c9989b2c230284b7177934889731753696
DevSagar007 <sagorkhan9294@gmail.com>
2025-07-20 23:38:49 +0600

    added card

--- src/Components/Recipe/Recipe.jsx
+++ src/Components/Recipe/Recipe.jsx
@@ -1,14 +1,92 @@
 
 
 const Recipe = ({ recipe }) => {
-  const { image, recipe_name, description } = recipe;
+  const { image, recipe_name, description, ingredients, calories } = recipe;
   return (
     <>
       <div className="card">
         <div className="card-body border border-[#D4D4D4] p-6 rounded-2xl">
-          <img className="w-full rounded-2xl" src={recipe.image} alt="" />
-          <h3 className="text-2xl font-bold mt-2">{recipe.recipe_name}</h3>
-          <p className="text-lg text-[#878787] mt-2">{recipe.description}</p>
+          <a href="#">
+            <img className="w-full rounded-2xl" src={recipe.image} alt="" />
+          </a>
+          <h3 className="text-xl font-bold mt-5">
+            <a href="#">{recipe.recipe_name}</a>
+          </h3>
+          <p className=" text-[#878787] mt-2 mb-4">{recipe.description}</p>
+          <ul className="border-b border-t border-[#ddd] pt-6 pb-6">
+            <h6 className="mb-2">Ingredients: 6</h6>
+            {recipe.ingredients.map((ingredient, index) => (
+              <li className="text-[#878787] mb-2 pl-3" key={index}>
+                {ingredient}
+              </li>
+            ))}
+          </ul>
+          <div className="flex gap-4 flex-wrap mt-2">
+            <div className="flex gap-1">
+              <span>
+                <svg
+                  width="20"
+                  height="20"
+                  viewBox="0 0 20 20"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M1 10C1 11.1819 1.23279 12.3522 1.68508 13.4442C2.13738 14.5361 2.80031 15.5282 3.63604 16.364C4.47177 17.1997 5.46392 17.8626 6.55585 18.3149C7.64778 18.7672 8.8181 19 10 19C11.1819 19 12.3522 18.7672 13.4442 18.3149C14.5361 17.8626 15.5282 17.1997 16.364 16.364C17.1997 15.5282 17.8626 14.5361 18.3149 13.4442C18.7672 12.3522 19 11.1819 19 10C19 8.8181 18.7672 7.64778 18.3149 6.55585C17.8626 5.46392 17.1997 4.47177 16.364 3.63604C15.5282 2.80031 14.5361 2.13738 13.4442 1.68508C12.3522 1.23279 11.1819 1 10 1C8.8181 1 7.64778 1.23279 6.55585 1.68508C5.46392 2.13738 4.47177 2.80031 3.63604 3.63604C2.80031 4.47177 2.13738 5.46392 1.68508 6.55585C1.23279 7.64778 1 8.8181 1 10Z"
+                    stroke="#282828"
+                    stroke-opacity="0.8"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M10 10H13.5"
+                    stroke="#282828"
+                    stroke-opacity="0.8"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                  <path
+                    d="M10 5V10"
+                    stroke="#282828"
+                    stroke-opacity="0.8"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </span>
+              <span className="text-[#535353]">{recipe.preparing_time}</span>
+            </div>
+            <div className="flex gap-1">
+              <span>
+                <svg
+                  width="14"
+                  height="19"
+                  viewBox="0 0 14 19"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M7 9C9 6.04 7 2 6 1C6 4.038 4.227 5.741 3 7C1.774 8.26 1 10.24 1 12C1 13.5913 1.63214 15.1174 2.75736 16.2426C3.88258 17.3679 5.4087 18 7 18C8.5913 18 10.1174 17.3679 11.2426 16.2426C12.3679 15.1174 13 13.5913 13 12C13 10.468 11.944 8.06 11 7C9.214 10 8.209 10 7 9Z"
+                    stroke="#282828"
+                    stroke-opacity="0.8"
+                    stroke-width="1.5"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  />
+                </svg>
+              </span>
+              <span className="text-[#535353]">{recipe.calories}</span>
+            </div>
+            <button
+              type="button"
+              className="btn h-12 px-6 bg-[#0be58a] font-semibold text-[#150B2B] flex items-center justify-center rounded-4xl"
+            >
+              Want to Cook
+            </button>
+          </div>
         </div>
       </div>
     </>
--- src/Components/Recipes/Recipes.jsx
+++ src/Components/Recipes/Recipes.jsx
@@ -11,10 +11,23 @@ const Recipes = () => {
 
   return (
     <div className="container mx-auto pt-10">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-10">
-        {recipes.map((recipe) => (
-          <Recipe key={recipe.id} recipe={recipe}></Recipe>
-        ))}
+      <div className="text-center mb-12 max-w-[820px] mx-auto">
+        <h1 className="text-4xl font-bold text-center mb-10">Our Recipes</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur. Proin et feugiat senectus
+          vulputate netus pharetra rhoncus. Eget urna volutpat curabitur
+          elementum mauris aenean neque
+        </p>
+      </div>
+      <div className="grid grid-cols-12 gap-10">
+        <div className="col-span-8">
+          <div className="grid grid-cols-2 gap-6">
+            {recipes.map((recipe) => (
+              <Recipe key={recipe.id} recipe={recipe}></Recipe>
+            ))}
+          </div>
+        </div>
+        <div className="col-span-4"></div>
       </div>
     </div>
   );
